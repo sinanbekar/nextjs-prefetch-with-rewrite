a809d2b0edea72641919b4dc72a217acd9173841
Sinan Bekar <sinanbekar.work@gmail.com>
2022-03-10 19:16:38 +0300

    switch to host header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,10 +10,14 @@ export default function Home() {
 
       <main>
         <ul>
-          <li><Link href="/without-rewrite">Without Rewrite</Link></li>
-          <li><Link href="/with-rewrite">With Rewrite</Link></li>
+          <li>
+            <Link href="/without-rewrite">Without Rewrite</Link>
+          </li>
+          <li>
+            <Link href="/with-rewrite">With Rewrite</Link>
+          </li>
         </ul>
       </main>
     </div>
-  )
+  );
 }
